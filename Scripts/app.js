20254ae1a0aeb7966500088e86d5cbae12e18e33
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-03-29 14:04:41 -0400

    added regex for phone number

--- Scripts/app.js
+++ Scripts/app.js
@@ -189,7 +189,12 @@
     function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
-    };
+  };
+
+  function isValidPhoneNumber(phoneNumber) {
+    var pattern = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/;
+    return pattern.test(phoneNumber);
+  }
 
     // Loads the Content for the Contact Page
     function LoadContactPage() {
@@ -211,7 +216,6 @@
             if(fullNameString.length < 2) {
               $("#FullNameErrorMessage").show(0, function(){
                 $(this).text("Full Name too short");
-                $(this).css("color", "red");
               });
               FullName.focus();
               FullName.select();
@@ -221,11 +225,23 @@
             }
         });
 
+        ContactNumber.change(function(){
+          if(!isValidPhoneNumber(ContactNumber.val())){
+             $("#ContactNumberErrorMessage").show(0, function(){
+                $(this).text("Invalid Phone Number");
+              });
+            ContactNumber.focus();
+            ContactNumber.select();
+          }
+          else {
+            $("#ContactNumberErrorMessage").hide();
+          }
+        })
+
         Email.change(function(){
           if(!isValidEmailAddress(Email.val())){
              $("#EmailErrorMessage").show(0, function(){
                 $(this).text("Invalid Email Address");
-                $(this).css("color", "red");
               });
             Email.focus();
             Email.select();
@@ -238,7 +254,7 @@
         // sendbutton click event
         SendButton.on("click", function (event) {
           // inside the sendbuton click event
-            event.preventDefault();
+            //event.preventDefault();
             //console.log(ContactNumber.val());
             //console.log(Email.val());
             //console.log(Message.val());
